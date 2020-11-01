65892452bca6f5eff7cc23b33634d3683d253b7b
santiago <smelomedina05@gmail.com>
2020-11-01 15:32:33 -0500

    styled login page

--- front/covidcontrol/src/app/app.module.ts
+++ front/covidcontrol/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { HomeComponent } from './main/pages/content/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/covidcontrol/src/app/main/pages/login/login.component.ts
+++ front/covidcontrol/src/app/main/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private loginForm: FormGroup;
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl()
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  public getLoginForm(): FormGroup {
+    return this.loginForm;
   }
 
 }
