2086da8a197a7f167e5306e7c4e848fc4069676b
santiago <smelomedina05@gmail.com>
2020-11-07 21:35:39 -0500

    fixed using absolute route and given guard for specific routes

--- front/covidcontrol/src/app/service/authentication/authentication.service.ts
+++ front/covidcontrol/src/app/service/authentication/authentication.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { JwtHelperService } from "@auth0/angular-jwt";
 import { environment } from 'src/environments/environment';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+
+  private jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor(private cookieService: CookieService) { }
 
@@ -15,15 +18,25 @@ export class AuthenticationService {
     this.cookieService.set(environment.AUTHENTICATION.COOKIE.USER, token);
   }
 
+  public isLogged(): boolean {
+    let result: boolean = false;
+    let cookie: string = this.getToken();
+    try {
+      if (!this.jwtHelper.isTokenExpired(cookie)) {
+        result = true;
+      }
+    } catch (error) {}
+    return result;
+  }
+
   public getToken(): string {
     return this.cookieService.get(environment.AUTHENTICATION.COOKIE.USER);
   }
 
   public getUser(): Object {
-    const jwt_helper: JwtHelperService = new JwtHelperService();
     let result: Object;
     try {
-      result = jwt_helper.decodeToken(this.getToken());
+      result = this.jwtHelper.decodeToken(this.getToken());
     } catch (error) {
       result = {};
     }
