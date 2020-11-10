32df0aaaa18d824fd41928a24b76e738b6caf587
santiago <smelomedina05@gmail.com>
2020-11-10 00:37:56 -0500

    started back with user microservice

---
+++ back/user/service/application/src/repository/user.py
@@ -0,0 +1,6 @@
+
+def authenticate(username, password):
+    return 1
+
+def getRoleByUsername(username):
+    return "Admin"
---
+++ back/user/service/application/src/service/user.py
@@ -0,0 +1,25 @@
+
+import src.repository.user as UserRepository
+from flask import request
+import jwt
+import src.constant.token as TOKEN
+import src.constant.response as RESPONSE
+
+def authenticate():
+    result = RESPONSE.EMPTY
+    data = eval(request.data.decode("utf-8"))
+    username, password = data["username"], data["password"]
+    response = UserRepository.authenticate(username, password)
+    if response != None:
+        role = UserRepository.getRoleByUsername(username)
+        token = jwt.encode(generatePayload(response, username, role), TOKEN.SECRET_KEY, algorithm="HS256")
+        result[TOKEN.NAME] = token
+    return result
+
+def generatePayload(id, username, role):
+    result = {
+        TOKEN.PAYLOAD['ID']: id,
+        TOKEN.PAYLOAD['USERNAME']: username,
+        TOKEN.PAYLOAD['ROLE']: role
+    }
+    return result
