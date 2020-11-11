099b43d5ca1e55fbb168c235bdda4e0046a53189
Juan Esteban Cardona <juanestebancg_@hotmail.com>
2020-11-10 20:36:26 -0500

    queries

--- makeDB.py
+++ makeDB.py
@@ -57,6 +57,22 @@ def getInactiveUsers(db):
     return ans
 
 
+def getUsersToActivate(db):
+    ans = list()
+    q1,q2 = db.healthEntity.find({'state':'I'}),db.establishment.find({'state':'I'})
+    for doc in q1:
+        ans.append({'docNum':doc['docNum'],'username':doc['username'],'name':doc['name'],
+        'city':doc['city'],'phoneNum':doc['phoneNum'],'neighHood':doc['neighHood'],'address':doc['address'],
+        'state':doc['state'],'totalDocts':doc['totalDocts'],'totalCap':doc['totalCap'],
+        'totalRes':doc['totalRes']})
+    for doc in q2:
+        ans.append({'docNum':doc['docNum'],'username':doc['username'],'name':doc['name'],
+        'city':doc['city'],'phoneNum':doc['phoneNum'],'neighHood':doc['neighHood'],'address':doc['address'],
+        'state':doc['state'],'totalCap':doc['totalCap'],'category':doc['category']})
+    return ans
+
+
+
 def getActiveUsers(db):
     ans = list()
     q1,q2,q3 = db.citizen.find({'state':'A'}),db.healthEntity.find({'state':'A'}),db.establishment.find({'state':'A'})
@@ -81,19 +97,20 @@ def getAllCitizens(db):
 def getAllHealthEn(db):
     ans,q = list(),db.healthEntity.find({})
     for doc in q:
-        ans.append({'docNum':doc['docNum'],'username':doc['username'],'names':doc['names'],'lastNames':doc['lastNames'],
+        ans.append({'docNum':doc['docNum'],'username':doc['username'],'name':doc['name'],
         'city':doc['city'],'phoneNum':doc['phoneNum'],'neighHood':doc['neighHood'],'address':doc['address'],
-        'gender':doc['gender'],'state':doc['state']})
+        'state':doc['state'],'totalDocts':doc['totalDocts'],'totalCap':doc['totalCap'],
+        'totalRes':doc['totalRes']})
     return ans     
 
 
 
 def getAllEstablishment(db):
-    ans,q = list(),db.healthEntity.find({})
+    ans,q = list(),db.establishment.find({})
     for doc in q:
-        ans.append({'docNum':doc['docNum'],'username':doc['username'],'names':doc['names'],'lastNames':doc['lastNames'],
+        ans.append({'docNum':doc['docNum'],'username':doc['username'],'name':doc['name'],
         'city':doc['city'],'phoneNum':doc['phoneNum'],'neighHood':doc['neighHood'],'address':doc['address'],
-        'gender':doc['gender'],'state':doc['state']})
+        'state':doc['state'],'totalCap':doc['totalCap'],'category':doc['category']})
     return ans     
 
 def makeUsersDB(client):
@@ -218,12 +235,6 @@ def makeExamsDB(client):
 
 
 
-
-
-    
-
-
-
 def main():
     #print(pymongo.version)
     client = MongoClient(port = 27017)    
@@ -245,19 +256,47 @@ def main():
     #db.healthEntity.delete_one({'state':'A'})
     #db.establishment.delete_one({'state':'I'})
 
+    if(not checkUsername("admin",db)):
+        registerUser("admin@admini.com","admin","admin","Admin",db)
+        registerAdmin(0,"424242","admin","angel","lee",db)
+
     if(not checkUsername("miguel22",db)):
         registerUser("m@m.com","admin","miguel22","Citizen",db)
         registerCitizen("1233","miguel22","miguel","adad","dada",0,"21212121","haei","calle 1","masculino","22/06","A",db)
 
  
     if(not checkUsername("miguel23",db)):
-        registerUser("m@m.com","admin","miguel23","Citizen",db)
+        registerUser("m2@m.com","admin","miguel23","Citizen",db)
         registerCitizen("1233","miguel23","miguel2","adad","dada",0,"21111","haei","calle 1","masculino","22/06","A",db)
     
-    print(checkUserCred("b","a",db))
-    print(getInactiveUsers(db))
-    print(getActiveUsers(db))
+    if(not checkUsername("clinica1",db)):
+        registerUser("clinica@m.com","admin","clinica1","ES",db)
+        #registerCitizen("1233","miguel23","miguel2","adad","dada",0,"21111","haei","calle 1","masculino","22/06","A",db)
+        registerHealthEn("123455","clinica1","medical","bogota",1,34,55,33,44,"ffa","cadd","31111","I",db)
+    
+    if(not checkUsername("clinica2",db)):
+        registerUser("clinic2@m.com","admin","clinica2","ES",db)
+        #registerCitizen("1233","miguel23","miguel2","adad","dada",0,"21111","haei","calle 1","masculino","22/06","A",db)
+        registerHealthEn("12345533","clinica2","medical","bogota",1,34,55,33,44,"ffa","cadd","31111","I",db)
+    
+    if(not checkUsername("EP1",db)):
+        registerUser("EP1@m.com","admin","EP1","EP",db)
+        registerEstablishment("535353","EP1","Eestab1","cali",1,34,"dada","calle 1","3232323","hola","I",db)
+    
+    if(not checkUsername("EP2",db)):
+        registerUser("EP2@m.com","admin","EP2","EP",db)
+        registerEstablishment("535353","EP2","Eestab1","cali",1,34,"dada","calle 1","3232323","hola","I",db)
+    
+    #print(checkUserCred("b","a",db))
+    #print(getInactiveUsers(db))
+    #print(getActiveUsers(db))
     print(getAllCitizens(db))
+    print("\n\n\n")
+    print(getAllHealthEn(db))
+    print("\n\n\n")
+    print(getAllEstablishment(db))
+    print("\n\n\n\n\n")
+    print(getUsersToActivate(db))
 
     print(db.list_collection_names())
 main()
