25c92a8a645d698dccd1e49316d5e931f22a33bc
Juan Esteban Cardona <juanestebancg_@hotmail.com>
2020-11-14 18:14:42 -0500

    Admin queries completed

--- back/DB/makeDB.py
+++ back/DB/makeDB.py
@@ -6,13 +6,59 @@ import random
 from datetime import datetime
 import time
 import pymongo
+import csv
 
 
+#-------------------------------------Ciudades y departamentos--------------------------------------------------
+def checkDept(db,name):
+    return True if db.department.find_one({"name":name}) != None else False
+
+def checkCity(db,name):
+    return True if db.city.find_one({"name":name}) != None else False
+
+def getIdDept(db,name):
+    return db.department.find_one({"name":name})['_id']
+
+def getIdCity(db,name):
+    return db.city.find_one({"name":name})['_id']
+
+def getNameCity(db,_id):
+    return db.city.find_one({"_id":_id})['name']
+
+def registerDept(db,name):
+    db.department.insert_one({"name":name})
+
+
+
+
+def registerCity(db,name,idDept):
+    db.city.insert_one({"name":name,"idDept":idDept})
+
+
+
+def initializeCountryParams(db,fileName):
+    with open(fileName,encoding="utf8") as file:
+        reader,i = csv.reader(file),0
+        for row in reader:
+            if i > 0:
+                fDept,fCity = checkDept(db,row[2]),checkCity(db,row[4])
+                if(not fDept and not fCity):
+                    registerDept(db,row[2])
+                    registerCity(db,row[4],getIdDept(db,row[2]))
+                elif(fDept and not fCity):
+                    registerCity(db,row[4],getIdDept(db,row[2]))
+            i += 1
+
+
+
+#-------------------------------------Ciudades y departamentos--------------------------------------------------
 def printDocument(doc):
     for k in doc.keys():
         print(f"{k}: {doc[k]}")
     print()
 
+
+#------------------------------------Chequear registro de usuario------------------------------------
 def checkUserCred(username,password,db):
     return str(db.user.find_one({'username':username,'password':password})['_id']) if db.user.find_one({'username':username,'password':password}) != None else None
 
@@ -23,14 +69,19 @@ def checkUsername(username,db):
 def checkEmail(email,db):
     return True if db.user.find_one({'email':email}) != None else False
 
+def checkRegistration(username,email,db):
+    return checkUsername(username,db) or checkEmail(email,db)
+
+
+#------------------------------------Chequear registro de usuario------------------------------------
 
 
 def getRole(username,db):
     return db.user.find_one({'username':username})['rol']
 
-def checkRegistration(username,email,db):
-    return checkUsername(username,db) or checkEmail(email,db)
-    
+
+
+#------------------------------------Registrar usuario------------------------------------
 
 def registerUser(email,password,username,rol,db):
     db.user.insert_one({'email':email,'password':password,'username':username,'rol':rol})
@@ -60,6 +111,44 @@ def registerEstablishment(docNum,username,name,city,docType,totalCap,address,nei
         'category':category,'state':state
     })
 
+
+def getDepts(db):
+    q,ans = db.department.find({}),list()
+    for doc in q:
+        ans.append(doc['name'])
+    return ans
+
+def getCitiesByDept(db,deptName):#obtiene todas las ciudades de un departamento
+    q,ans =  db.city.find({'idDept' : getIdDept(db,deptName)}),list()
+    for doc in q:
+        ans.append(doc['name'])
+    return ans
+
+def getCategories(db):
+    q,ans = db.category.find({}),list()
+    for doc in q:
+        ans.append(doc['name'])
+    return ans
+
+def getNeighHoodsByCity(db,cityName):#obtiene todos los barrios de una ciudad
+    q,ans = db.neighborHood.find({'city':getIdCity(db,cityName)}),list()
+    for doc in q:
+        ans.append(doc['name'])
+    return ans
+
+def getDocTypes(db):
+    q,ans = db.documentType.find({}),list()
+    for doc in q:
+        ans.append(doc['name'])
+    return ans
+
+
+
+#------------------------------------Registrar usuario------------------------------------
+
+
+
+#------------------------------------Funciones administrador------------------------------------
 
 def getInactiveUsers(db):
     ans = list()
@@ -127,7 +216,77 @@ def getAllEstablishment(db):
         ans.append({'docNum':doc['docNum'],'username':doc['username'],'name':doc['name'],
         'city':doc['city'],'phoneNum':doc['phoneNum'],'neighHood':doc['neighHood'],'address':doc['address'],
         'state':doc['state'],'totalCap':doc['totalCap'],'category':doc['category']})
-    return ans     
+    return ans
+
+
+def getAllUsers(db):
+    return getAllCitizens(db)+getAllHealthEn(db)+getAllEstablishment(db)
+
+    
+def getAllDocTypes(db):
+    q,ans = db.documentType.find({}),list()
+    for doc in q:
+        ans.append(doc['name'])
+    return ans
+
+
+def getAllExams(db):
+    q,ans = db.exam.find({}),list()
+    for doc in q:
+        ans.append({"docNumCi":doc['docNumCi'],"docNumHe":doc['docNumHe'],"citizensName":doc['citizensName'],"result":doc['result']})
+    return ans
+
+def getAllEntries(db):#Mostrar el nombre de la persona y el del establecimiento ? 
+    q,ans = db.entries.find({}),list()
+    for doc in q:
+        ans.append({'docNumCi':doc['docNumCi'],'docNumEs':doc['docNumEs'],'temperature':doc['temperature'],
+        'date':doc['date'],'time':doc['time'],'mask':doc['mask'],'ans':doc['ans'],'description':doc['description']})
+    return ans
+def getAllNeighHoods(db):
+    q,ans = db.neighborHood.find({}),list()
+    for doc in q:
+        ans.append({'name':doc['name'],'city': getNameCity(db,doc['city'])})
+    return ans
+
+def registerNeighHood(db,name,cityName):
+    db.neighborHood.insert_one({'name':name,'city':getIdCity(db,cityName)})
+
+def registerDocType(db,name):
+    db.documentType.insert_one({'name':name})
+
+
+def registerQuarantine(db,days):
+    db.quarantine.drop()
+    db.quarantine.insert_one({'days':days})
+
+
+
+def setCitizenState(db,username,newState):#Revisar!!
+    db.citizen.update_one({'username':username},{'$set':{'state':newState}},upsert = False)
+
+def setHealthEnState(db,username,newState):#Revisar!!
+    db.healthEntity.update_one({'username':username},{'$set':{'state':newState}},upsert = False)
+
+
+def setEstablishmentState(db,username,newState):#Revisar!!
+    db.establishment.update_one({'username':username},{'$set':{'state':newState}},upsert = False)
+
+
+
+#------------------------------------Funciones administrador---------------------------------------
+
+
+
+#------------------------------------Funciones ciudadano---------------------------------------
+#TODO
+
+
+#------------------------------------Funciones ciudadano---------------------------------------
+
+
+
+#------------------------------------Declarar las bases de datos------------------------------------
+
 
 def makeUsersDB(client):
     db = client['UsersDB']
@@ -196,7 +355,7 @@ def makeParametersDB(client):
     col = db['quarantine']
     col.insert_one({
         'days':'',
-        'state':''
+        'state':'' #si o no??
     })
 
 
@@ -210,7 +369,7 @@ def makeEntryDB(client):
         'docNum':'',
         'entriesReg':[]
     })
-    col = db['entries']
+    col = db['entry']
     col.insert_one({
         'docNumCi':'',
         'docNumEs':'',
@@ -248,6 +407,10 @@ def makeExamsDB(client):
         'citizensLastNames':'',
         'result':''
     })
+
+
+#------------------------------------Declarar las bases de datos------------------------------------
+
 
 
 
@@ -302,7 +465,7 @@ def main():
     if(not checkRegistration("EP2","EP2@m.com",db)):
         registerUser("EP2@m.com","admin","EP2","EP",db)
         registerEstablishment("535353","EP2","Eestab1","cali",1,34,"dada","calle 1","3232323","hola","I",db)
-    
+    """
     #print(checkUserCred("b","a",db))
     #print(getInactiveUsers(db))
     #print(getActiveUsers(db))
@@ -316,6 +479,13 @@ def main():
     print("\n\n\n\n\n")
     print(getUsersToActivate(db))
     print(db.list_collection_names())
+    """
+   
+    db = client.ParametersDB
+    #db.city.drop()
+    #db.department.drop()
+    #initializeCountryParams(db,"dataset.csv")
+    print(getCitiesByDept(db,"Antioquia"))
     
 main()
     
