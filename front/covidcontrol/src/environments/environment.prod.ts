4b53e318a76a35960b5b9ed8ae1bb54416823871
Santiago <smelomedina05@gmail.com>
2020-12-04 06:16:25 -0500

    dist with files 2

--- front/covidcontrol/src/environments/environment.prod.ts
+++ front/covidcontrol/src/environments/environment.prod.ts
@@ -1,15 +1,23 @@
 
-import { environment as ENV } from './environment';
+import { CONSTANTS as PETITION } from '../../src/app/constant/petition/petition.constants';
+import { CONSTANTS as MENU } from '../../src/app/constant/menu.constant';
+import { CONSTANTS as FUNCTION } from '../../src/app/constant/function.constant';
+import { CONSTANTS as ACTION } from '../../src/app/constant/action.constant';
+import { CONSTANTS as AUTHENTICATION } from '../../src/app/constant/authentication.constant';
+import { CONSTANTS as ROUTING } from '../../src/app/constant/routing.constant';
+import { CONSTANTS as VALUE } from '../../src/app/constant/value/value.constant';
+import { CONSTANTS as TITLES } from '../../src/app/constant/title.constant';
+import { CONSTANTS as HTTP_CODES } from '../../src/app/constant/http_code.constant';
 
 export const environment = {
   production: true,
-  PETITION: ENV.PETITION,
-  MENU: ENV.MENU,
-  FUNCTION: ENV.FUNCTION,
-  ACTION: ENV.ACTION,
-  AUTHENTICATION: ENV.AUTHENTICATION,
-  ROUTING: ENV.ROUTING,
-  VALUE: ENV.VALUE,
-  TITLES: ENV.TITLES,
-  HTTP_CODES: ENV.HTTP_CODES
+  PETITION: PETITION,
+  MENU: MENU,
+  FUNCTION: FUNCTION,
+  ACTION: ACTION,
+  AUTHENTICATION: AUTHENTICATION,
+  ROUTING: ROUTING,
+  VALUE: VALUE,
+  TITLES: TITLES,
+  HTTP_CODES: HTTP_CODES
 };
