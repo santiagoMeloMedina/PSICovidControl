f824b833270be441d080c60c1c6b4ddfb94c0dc4
Santiago <smelomedina05@gmail.com>
2020-12-03 21:10:50 -0500

    added ips

--- front/covidcontrol/src/app/constant/petition/petition.constants.ts
+++ front/covidcontrol/src/app/constant/petition/petition.constants.ts
@@ -2,9 +2,9 @@
 import { CONSTANTS as ENDPOINTS } from './endpoint.constant';
 
 export const CONSTANTS = {
-    API: "http://localhost:2100",
-    API2: "http://localhost:5100",
-    API3: "http://localhost:3100",
-    API4: "http://localhost:4100",
+    API: "http://18.225.7.201:2100",
+    API2: "http://3.139.81.181:5100",
+    API3: "http://52.14.42.144:3100",
+    API4: "http://18.216.150.237:4100",
     ENDPOINTS: ENDPOINTS
 }
