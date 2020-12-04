6412eda362b91fc97656046cec5c1ab295768aea
Santiago <smelomedina05@gmail.com>
2020-12-04 04:47:06 -0500

    added production environment for front

--- front/covidcontrol/src/environments/environment.prod.ts
+++ front/covidcontrol/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+
+import { environment as ENV } from './environment';
+
 export const environment = {
-  production: true
+  production: true,
+  PETITION: ENV.PETITION,
+  MENU: ENV.MENU,
+  FUNCTION: ENV.FUNCTION,
+  ACTION: ENV.ACTION,
+  AUTHENTICATION: ENV.AUTHENTICATION,
+  ROUTING: ENV.ROUTING,
+  VALUE: ENV.VALUE,
+  TITLES: ENV.TITLES,
+  HTTP_CODES: ENV.HTTP_CODES
 };
